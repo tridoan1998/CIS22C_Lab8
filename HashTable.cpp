248ee145ba0376bb2feca700924a0ff9a7f044a7
Tri Doan <tridoan1998@gmail.com>
2017-11-17 15:21:52 -0800

    Add files via upload

--- HashTable.cpp
+++ HashTable.cpp
@@ -7,9 +7,37 @@
 //============================================================================
 
 #include <iostream>
+#include <cstddef> //for NULL
+#include <assert.h>
+#include <string>
+#include "HashTable.h"
+#include "Book.h"
+#include "BST.h"
+
 using namespace std;
 
-int main() {
-	cout << "!!!Hddfdfdfdfeldfdfdflodfdf Wodffdrld!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+int HashTable::hash(string key) const
+{
+
+	int index;
+	int sum = 0;
+	for(unsigned int i = 0; i < key.length(); i++)
+	{
+		sum += (int)key[i];
+	}
+	index = sum % SIZE;
+
+	return index;
 }
+
+int HashTable::countBucket(int index) const
+{
+	assert(0 <= index && index < SIZE);
+	return Table[index].getSize();
+}
+
+
+
+//counts the number of Books at this index
+//returns the count
+//pre: 0<= index < SIZE
