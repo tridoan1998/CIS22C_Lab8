ee892403d1241dd015eeee6e791b171b50e51c6c
Tri Doan <tridoan1998@gmail.com>
2017-11-19 22:18:29 -0800

    Add files via upload

--- HashTable.cpp
+++ HashTable.cpp
@@ -49,11 +49,46 @@ void HashTable::insert(Book b)
 {
 	string key = b.get_title() + b.get_author();
 	int index = hash(key);
-	if(Table[index].isEmpty())
-		insert(b);
-	else if()
+	Table[index].insert(b);
 }
 
-//inserts a new book into the table
-//calls the hash function on the key to determine
-//the correct bucket
+void HashTable::remove(Book b)
+{
+	string key = b.get_title() + b.get_author();
+	int index = hash(key);
+	Table[index].remove(b);
+}
+
+void HashTable::printBucket(ostream& out, int index) const
+{
+	assert(0 <= index && index <= SIZE);
+	Table[index].inOrderPrint(out);
+
+}
+
+//Prints all the books at index according to inOrderPrint
+ //pre: 0<= index < SIZE
+ //Should print according to the following formula:
+ //Prints each book at that index in the format:
+ //<title> by <author>
+ //$<X.XX>
+ //ISBN#: <isbn>
+ //followed by a blank line
+
+void HashTable::printTable(ostream& out) const
+{
+
+}
+//Prints the first book at each index
+//along with a count of the total books
+//at each index by calling count_bucket
+//as a helper function
+//Prints in the format:
+//Books in the Catalogue:
+//<new line>
+//Group <bucket>
+//<title> by <author>
+//$<X.XX>
+//ISBN: <isbn>
+//+<number of elements at this index> -1 more similar book(s)
+//<new line><new line><new line>
